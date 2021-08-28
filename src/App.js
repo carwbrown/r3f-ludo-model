53d283cdb2effbe5ab461f781874b0f2f39972a6
Carson Brown <carsonb@ifit.com>
2021-08-28 13:33:10 -0400

    arkanoid game, with score keeper, and walls

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import React, { useState } from "react";
+import { Canvas, useFrame, useThree } from "react-three-fiber";
+import { Physics, useSphere, useBox, usePlane } from "@react-three/cannon";
+import { Html } from "@react-three/drei";
+import "./index.css";
+
+const Ball = ({ resetScore }) => {
+  const { viewport } = useThree();
+  const [ref, api] = useSphere(() => ({
+    args: 0.5,
+    mass: 0.1,
+    position: [0, 0, 0],
+  }));
+
+  usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    position: [0, -viewport.height, 0],
+    onCollide: () => {
+      api.position.set(0, 0, 0);
+      api.velocity.set(0, 0, 0);
+      resetScore();
+    },
+  }));
+
+  return (
+    <mesh ref={ref}>
+      <sphereBufferGeometry args={[0.5, 32, 32]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  );
+};
+
+const Wall = () => {
+  const { viewport } = useThree();
+  const [ref] = usePlane(() => ({
+    args: [1, viewport.height],
+    rotation: [0, -Math.PI / 2, 0],
+    position: [viewport.width / 2, 0, 0],
+  }));
+
+  return (
+    <mesh ref={ref}>
+      <planeBufferGeometry attach="geometry" args={[1, viewport.height + 2]} />
+      <meshPhongMaterial attach="material" color="black" opacity={0.5} />
+    </mesh>
+  );
+};
+
+const Wall2 = () => {
+  const { viewport } = useThree();
+  const [ref] = usePlane(() => ({
+    args: [1, viewport.height],
+    rotation: [0, Math.PI / 2, 0],
+    position: [-viewport.width / 2, 0, 0],
+  }));
+
+  return (
+    <mesh ref={ref}>
+      <planeBufferGeometry attach="geometry" args={[1, 60]} />
+      <meshPhongMaterial attach="material" color="black" opacity={0.5} />
+    </mesh>
+  );
+};
+
+const Ceiling = () => {
+  const { viewport } = useThree();
+  const [ref] = usePlane(() => ({
+    rotation: [Math.PI / 2, 0, 0],
+    position: [0, viewport.height / 2, 0],
+  }));
+
+  return (
+    <mesh ref={ref}>
+      <planeBufferGeometry attach="geometry" args={[viewport.width, 2]} />
+      <meshPhongMaterial attach="material" color="black" opacity={0.5} />
+    </mesh>
+  );
+};
+
+const Paddle = () => {
+  const [ref, api] = useBox(() => ({ args: [2, 0.5, 1] }));
+
+  useFrame((state) => {
+    api.position.set(
+      (state.mouse.x * state.viewport.width) / 2,
+      -state.viewport.height / 2 + 1,
+      0,
+    );
+    api.rotation.set(0, 0, (state.mouse.x * Math.PI) / 5);
+  });
+
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry args={[2, 0.5, 1]} />
+      <meshStandardMaterial color="lightblue" />
+    </mesh>
+  );
+};
+
+const Enemy = ({ color, args = [2, 0.5, 1], setScore, ...props }) => {
+  const [ref, api] = useBox(() => ({
+    args,
+    ...props,
+    onCollide: () => {
+      api.position.set([0, 0, 0]);
+      setScore();
+    },
+  }));
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry args={args} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+
+const useScore = (startScore = 0) => {
+  const [score, setScore] = useState(startScore);
+  const incrementScore = () => setScore((score) => score + 1);
+  const resetScore = () => setScore(() => 0);
+  return [score, incrementScore, resetScore];
+};
+
+function App() {
+  const [score, incrementScore, resetScore] = useScore(0);
+
+  return (
+    <Canvas camera={{ position: [0, 0, 20], fov: 50 }}>
+      <ambientLight intensity={0.3} />
+      <pointLight position={[10, 10, 5]} />
+      <pointLight position={[-10, -10, -5]} />
+      <Physics
+        gravity={[0, -30, 0]}
+        defaultContactMaterial={{ restitution: 1.1 }}
+      >
+        <Wall />
+        <Wall2 />
+        <Ceiling />
+        <Ball resetScore={resetScore} />
+        <Paddle />
+        <Enemy color="orange" position={[2, 1, 0]} setScore={incrementScore} />
+        <Enemy
+          color="hotpink"
+          position={[-2, 3, 0]}
+          setScore={incrementScore}
+        />
+        <Html prepend distanceFactor={10}>
+          <div className="content">{score}</div>
+        </Html>
+      </Physics>
+    </Canvas>
+  );
+}
+
+export default App;
---
+++ todo.md
@@ -0,0 +1,23 @@
+- [x] go through fundamentals https://github.com/pmndrs/react-three-fiber#fundamentals
+  - [ ] https://threejsfundamentals.org/ - currently here https://threejsfundamentals.org/threejs/lessons/threejs-scenegraph.html
+  - [ ] simple tutorial https://codeworkshop.dev/blog/2019-12-31-learn-the-basics-of-react-three-fiber-by-building-a-three-point-lighting-setup/
+  - [ ] https://discoverthreejs.com/
+  - [ ] https://discoverthreejs.com/tips-and-tricks/
+  - [ ] intermediate https://blog.mozvr.com/threejs-intermediate-skill-tutorials/
+- [ ] youtube video playlist https://www.youtube.com/watch?v=YyqBdN71nFs
+- [ ] WebGL YT vidya playlist https://www.youtube.com/watch?v=wbI55mc7ksE
+- [ ] playlist https://www.youtube.com/watch?v=Qs9A3XIjbg4&list=PLgcPxVODYXGIHdoZ_QY4OBdlLDFM7nzBr
+- [ ] build minecraft https://www.youtube.com/watch?v=ZnXKmODEFHA
+- [ ] build starfox (reactfox) https://www.youtube.com/watch?v=ADBnmJ1Z_18
+- [ ] game dev presentation https://www.youtube.com/watch?v=nHaiLWUaWWw
+- [x] arkanoid tutorial https://www.youtube.com/watch?v=Go3QxQG6RK8
+- [ ] Udemy course
+  - [ ] https://www.udemy.com/course/create-a-3d-multi-player-game-using-threejs-and-socketio/learn/lecture/13738764#overview - socket io 3.js
+
+examples
+
+- [ ] https://codesandbox.io/s/arkanoid-under-60-loc-66cd7 arkanoid
+- [ ] https://codesandbox.io/s/ragdoll-physics-wdzv4 ragdoll man
+- [ ] https://codesandbox.io/s/ping-pong-0mgum ping pong
+- [ ] https://codesandbox.io/s/space-game-i2160 - space game
+- [ ] https://dev.to/flagrede/making-a-2d-rpg-game-with-react-tree-fiber-4af1 - 2d rpg game
